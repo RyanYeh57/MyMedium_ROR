3744cad6f558851bb588f26c4d2c767437717c2f
Ryan Yeh <ej68433221ik@gmail.com>
2022-06-25 20:58:16 +0800

    finish stories edit & update

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,10 @@
 class StoriesController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_story, only: [:edit, :update, :destroy]
+  
+  def index
+    @stories = current_user.stories.order(created_at: :desc)
+  end
 
   def new
     @story = current_user.stories.new
@@ -9,18 +14,32 @@ class StoriesController < ApplicationController
     @story = current_user.stories.new(story_params)
     
     if @story.save
-      redirect_to stories_path, notice: "故事新增成功"
+      redirect_to stories_path, notice: '故事更新成功'
     else
       render :new
     end
   end
 
-  def index
+  def edit
+  end
+
+  def update
+    if @story.update(story_params)
+      redirect_to stories_path, notice: '故事更新成功'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
   end
 
   private
+  def find_story
+    @story = current_user.stories.find(params[:id])
+  end
+
   def story_params
     params.require(:story).permit(:title, :content)
   end
-
 end
