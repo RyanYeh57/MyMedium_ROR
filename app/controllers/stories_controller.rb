79fda698439000ca7bf185dd0705c6b86da235de
Ryan Yeh <ej68433221ik@gmail.com>
2022-06-26 21:35:22 +0800

    finish save_as_draft & publish

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -12,9 +12,14 @@ class StoriesController < ApplicationController
 
   def create
     @story = current_user.stories.new(story_params)
+    @story.status = 'published' if params[:publish]
     
     if @story.save
-      redirect_to stories_path, notice: '故事更新成功'
+      if params[:publish]
+        redirect_to stories_path, notice: '故事發佈成功'
+      else
+        redirect_to edit_story_path(@story), notice: '故事已儲存'
+      end
     else
       render :new
     end
@@ -24,16 +29,23 @@ class StoriesController < ApplicationController
   end
 
   def update
+    @story.status = 'published' if params[:publish]
     if @story.update(story_params)
-      redirect_to stories_path, notice: '故事更新成功'
+      if params[:publish]
+        redirect_to stories_path, notice: '故事發佈成功'
+      else
+        redirect_to edit_story_path(@story), notice: '故事已儲存'
+      end
     else
       render :edit
     end
   end
 
   def destroy
+    @story.destroy
+    redirect_to stories_path, notice: '故事已刪除'
   end
-
+  
   private
   def find_story
     @story = current_user.stories.find(params[:id])
