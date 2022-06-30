ee7e20eb5bfab9c0ba2de2fa5a9207213c730c67
Ryan Yeh <ej68433221ik@gmail.com>
2022-06-30 16:34:33 +0800

    pages.index add  story list

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
         registrations: 'users/registrations'
       }
+  
+  #/@user/story.title
+  get '@:username/:story_id', to: 'pages#show', as: 'story_page'
+
+  #/@user
+  get '@:username', to: 'pages#user', as: 'user_page'
 
   resources :stories
 
